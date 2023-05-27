46cfccb073cf65f0e7c1b66b1f38f2ee0a4a3b18
IkbalFauzi20 <ikbalf86@gmail.com>
2023-05-28 01:49:27 +0700

    menambahkan fitur upload gambar melalui files di tambah.php

--- kuliah/pertemuan13/functions.php
+++ kuliah/pertemuan13/functions.php
@@ -36,7 +36,21 @@ function tambah($data)
   $npm = htmlspecialchars($data["npm"]);
   $email = htmlspecialchars($data["email"]);
   $jurusan = htmlspecialchars($data["jurusan"]);
-  $gambar = htmlspecialchars($data["gambar"]);
+  //$gambar = htmlspecialchars($data["gambar"]);
+
+  if (empty($nama) || empty($npm) || empty($email) || empty($jurusan)) {
+    echo "<script>
+          alert('Pastikan semua data telah di isi');
+          document.location.href = 'tambah.php';
+          </script>";
+    return false;
+  }
+
+  // upload gambar
+  $gambar = upload();
+  if (!$gambar) {
+    return false;
+  }
 
   $query = "INSERT INTO 
   mahasiswa
@@ -46,6 +60,59 @@ function tambah($data)
   mysqli_query($conn, $query);
   echo mysqli_error($conn);
   return mysqli_affected_rows($conn);
+}
+
+function upload()
+{
+  $namaFile = $_FILES['gambar']['name'];
+  $tipeFile = $_FILES['gambar']['type'];
+  $ukuranFile = $_FILES['gambar']['size'];
+  $error = $_FILES['gambar']['error'];
+  $tmpName = $_FILES['gambar']['tmp_name'];
+
+  // cek apakah tidak ada gambar yg diupload
+  if ($error === 4) {
+    echo "<script>
+    alert('pilih gambar terlebih dahulu');
+    </script>";
+    return false;
+  }
+
+  // cek ekstensi gambar
+  $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+  $ekstensiGambar = explode('.', $namaFile);
+  $ekstensiGambar = strtolower(end($ekstensiGambar));
+  if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+    echo "<script>
+    alert('yang anda upload bukan gambar');
+    </script>";
+    return false;
+  }
+
+  // cek tipe file
+  if ($tipeFile != 'image/jpeg' && $tipeFile != 'image/png') {
+    echo "<script>
+    alert('yang anda upload bukan gambar');
+    </script>";
+    return false;
+  }
+
+  // cek jika ukurannya terlalu besar
+  if ($ukuranFile > 5000000) {
+    echo "<script>
+    alert('Ukuran gambar terlalu besar');
+    </script>";
+    return false;
+  }
+
+  //gambar siap diupload
+  // generate nama gambar baru
+  $namaFileBaru = uniqid();
+  $namaFileBaru .= '.';
+  $namaFileBaru .= $ekstensiGambar;
+
+  move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
+  return $namaFileBaru;
 }
 
 function hapus($id)
@@ -132,7 +199,7 @@ function registrasi($data)
 
 
   // jika username dan password yg diinput kosong
-  if (empty($username) || empty($password || empty($password2))) {
+  if (empty($username) || empty($password) || empty($password2)) {
     echo "<script>
           alert('Username dan Password tidak boleh kosong!');
           document.location.href = 'registrasi.php';
---
+++ kuliah/pertemuan13/tambah.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+
+// apakah user sudah login atau belum
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+// cek apakah tombol tambah sudah ditekan atau belum
+if (isset($_POST["tambah"])) {
+  if (tambah($_POST) > 0) {
+    echo "<script>
+        alert('Data Berhasil Ditambahkan');
+        document.location.href = 'index.php';
+        </script>";
+  } else {
+    echo "alert('Data Gagal Ditambahkan!')";
+  }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Form Tambah Data Mahasiswa</h3>
+  <form action="" method="post" enctype="multipart/form-data">
+    <ul>
+      <li>
+        <label>
+          Nama
+          <input type="text" name="nama" placeholder="nama" required autofocus>
+        </label>
+      </li>
+      <li>
+        <label>
+          NPM
+          <input type="text" name="npm" placeholder="npm" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Email
+          <input type="text" name="email" placeholder="email" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Jurusan
+          <input type="text" name="jurusan" placeholder="jurusan" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Gambar
+          <input type="file" name="gambar" placeholder="gambar">
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="tambah">Tambah Data!</button>
+      </li>
+    </ul>
+  </form>
+
+</body>
+
+</html>
